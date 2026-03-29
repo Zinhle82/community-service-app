8db47e74bc7b99273f1ed14ab5aea7bc748abe8f
Zinhle82 <zwanezinhle864@gmail.com>
2026-03-29 15:59:16 +0300

    Final version

--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,20 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Issue
 
 def report_issue(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        location = request.POST.get('location')
+
+        Issue.objects.create(
+            title=title,
+            description=description,
+            location=location
+        )
+
+        messages.success(request, "Issue reported successfully!")
+        return redirect('/reports/')   
+
     return render(request, 'report_issue.html')
--- tracking/views.py
+++ tracking/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from reports.models import Issue
 
 def view_reports(request):
-    return render(request, 'view_reports.html')
+    issues = Issue.objects.all().order_by('-created_at')  
+    return render(request, 'view_reports.html', {'issues': issues})
